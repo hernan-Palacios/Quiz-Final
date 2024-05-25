85a0cec0ab99fc289d1896eb2f1c7535468c8a0c
hernan-Palacios <hernanpalacios147736@correo.itm.edu.co>
2024-05-24 20:09:46 -0500

    Ejercio propuesto

--- Program.cs
+++ Program.cs
@@ -2,6 +2,39 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Quiz final
+        /*Peso: 100%: Tablero numérico 
+Desarrollar un programa en C# que simule un juego de suma y movimiento en un tablero NxN. El 
+objetivo del juego es mover el cero "0" a través del tablero para sumar los valores de las casillas y 
+acumular puntos, evitando los obstáculos.  
+Requisitos del Programa: 
+1. Inicialización del Tablero: 
+ El tablero es una matriz de NxN. 
+ La posición inicial es en [0,0] y debe estar representada por el número cero 0. 
+ Las demás posiciones del tablero deben ser llenadas con números aleatorios entre 1 y 9. 
+2. Menú de Opciones: 
+ Implementar un menú con las siguientes opciones: 
+Mover Derecha 
+Mover Izquierda 
+Mover Arriba 
+Mover Abajo 
+Esc para Salir del Juego 
+3. Movimientos del Jugador: 
+ Permitir al jugador mover el "0" en la dirección indicada por el menú usando las teclas de 
+dirección o las teclas WASD. 
+ Si el usuario mueve el “0” con la tecla Flecha Arriba, se moverá, y si la mueve con W 
+también debe moverse sin problema. Recuerden WASD es: Tecla W hacia arriba, tecla A 
+hacia la izquierda, tecla S hacia abajo y tecla D hacia la derecha. 
+ El jugador no puede moverse fuera de los límites del tablero. 
+ Cada vez que el jugador se mueva a una nueva casilla, el valor de la casilla debe sumarse al 
+puntaje y la casilla debe ser reemplazada por un 0. 
+4. Mostrar el Tablero: 
+ Mostrar el tablero actualizado después de cada movimiento, indicando la posición actual del 
+jugador. 
+ Mostrar el puntaje acumulado del jugador. */
+
+
+
+
     }
 }
